2937494106dba39551b4f039f09596ab4f468f18
Scorpion-rep <86839837+Scorpion-rep@users.noreply.github.com>
2021-09-20 22:38:15 +0800

    repo cloned

---
+++ script.js
@@ -0,0 +1,40 @@
+
+//Show the landing page
+
+//when start button is clicked
+// starts
+
+// 1. Timer will start
+// 2. question page shows up
+
+
+
+//Question page
+//when we click on choice button
+
+//need to check if button clicked is the correct choice
+
+//is correct:
+//show a message or pop up box that the choice is correct
+//move on to the next question
+
+//if wrong:
+//move to next question
+//deduct time from timer
+//show a message that the choice is incorrect
+
+// When we end the game
+// 1. if user on the last question
+// after user click on the choice
+// 2. timer runs out
+
+
+// What happens after the game finished
+//1. stop the timer
+//2. show the landing page
+
+//Ending Page-High score page
+//when form is submitted
+//we want to save the highscore
+//direct to the landing page
+
